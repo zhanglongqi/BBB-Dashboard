e7ec1711634ba2d49a790a7be9eef22da211516d
longqi zhang <benhuan@126.com>
2015-04-02 16:57:54 +0000

    update CSS style

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import tornado.web
 
 from tornado.options import define, options
 
-define("port", default=8888, help="run on the given port", type=int)
+define("port", default=os.environ["PORT"], help="run on the given port", type=int)
 
 
 class IndexHandler(tornado.web.RequestHandler):
