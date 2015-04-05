ad4f6ce9afaf5599c4a471d4eb08c62095dcb419
ZhangLongQi <longqi90@gmail.com>
2015-04-05 11:51:39 +0800

    change port back to local

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import tornado.web
 
 from tornado.options import define, options
 
-define("port", default=os.environ["PORT"], help="run on the given port", type=int)
+define("port", default=8000, help="run on the given port", type=int)
 
 
 class IndexHandler(tornado.web.RequestHandler):
